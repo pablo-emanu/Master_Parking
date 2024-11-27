2973979caa49dcf2ea9e2f81ee9ef0ab7cb11f0e
Pablo Emanuel <emanuelpabloferreira1@gmail.com>
2024-11-26 21:48:38 -0300

    Removendo Interfaces e classes de persistencias para nova aplicação

--- src/PersistenciaCliente.java
+++
@@ -1,8 +0,0 @@
-public interface PersistenciaCliente {
-
-    public void salvar(Cliente cliente);
-    public String removerPorCpf(String cpf);
-    public Cliente buscarPorCpf(String cpf);
-    public void atualizar(Cliente cliente, String cpf);
-
-}
